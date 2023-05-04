99f2e96de6702164a58f7a3a9740db2e6843abaa
Violet <violet@viomck.com>
2023-05-04 08:43:02 -0700

    guild count

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-	print(f'We have logged in as {client.user}')
+	print(f'We have logged in as {client.user} ({client.guilds.count} guilds)')
 
 @client.event
 async def on_message(message: discord.Message):
